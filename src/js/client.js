a8261d439575a1fb15607892f36bab04ed306135
denniscual <mamayandpapay@gmail.com>
2016-12-08 21:36:04 +0800

    initial adding

--- src/js/client.js
+++ src/js/client.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+class App extends React.Component{
+
+    render(){
+        return(
+          <h1>This is React Router Sample</h1>
+        );
+    }
+
+}
+
+document.getElementById();
