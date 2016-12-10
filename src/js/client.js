a5c3b621413565e848c492a73542b90c95bcea6c
denniscual <mamayandpapay@gmail.com>
2016-12-10 11:52:25 +0800

    commit

--- src/js/client.js
+++ src/js/client.js
@@ -4,39 +4,43 @@ import { render } from 'react-dom';
 import {Header} from './components/header'
 import Button from './components/button'
 
+
 class App extends React.Component{
+
 
     constructor(){
         super();
-        this.hideText = "Hide the subtitle text";
-        // this.displayText = "Show the subtitle of heading text";
         this.state = {
-            stateSubTextComp: true,
-            subText: "Hide the subtitle text"
+            isToggleText: true,
+            isToggleSubtitle: true
         };
+        this.handleClick = this.handleClick.bind(this);
     }
 
 
-    onStateSubText(newText) {
-        this.setState({
-            stateSubTextComp: !this.state.stateSubTextComp,
-            subText: newText
-        });
 
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleText: !prevState.isToggleText,
+            isToggleSubtitle: !prevState.isToggleSubtitle
+        }));
     }
-
 
     // composition let you described a name for your element props.
     render(){
-        {/*let subTextComp = "";*/}
-        // if(this.state.stateSubTextComp){
-        //     subTextComp = (
-        //         <p>
-        //             Proin eget tortor risus. Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta. Curabitur aliquet quam id dui posuere blandit. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus nibh. Pellentesque in ipsum id orci porta dapibus. Nulla porttitor accumsan tincidunt. Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada.
-        //         </p>
-        //     );
-        //
-        // }
+        let headerComp = "";
+        if(this.state.isToggleSubtitle){
+            headerComp = (
+                <p>
+                    Proin eget tortor risus. Vivamus suscipit tortor eget felis porttitor volutpat. Cras
+                    ultricies ligula sed magna dictum porta. Curabitur aliquet quam id dui posuere blandit. Cras
+                    ultricies ligula sed magna dictum porta. Sed porttitor lectus nibh. Pellentesque in ipsum id
+                    orci porta dapibus. Nulla porttitor accumsan tincidunt. Cras ultricies ligula sed magna
+                    dictum porta. Donec sollicitudin molestie malesuada.
+                </p>
+            );
+        }
         return(
             <div>
                 <Header
@@ -44,21 +48,26 @@ class App extends React.Component{
                         <h3>This is the header. This element is passed like a props data.</h3>
                     }
                     subText = {
-                        <p>
-                            Proin eget tortor risus. Vivamus suscipit tortor eget felis porttitor volutpat. Cras
-                            ultricies ligula sed magna dictum porta. Curabitur aliquet quam id dui posuere blandit. Cras
-                            ultricies ligula sed magna dictum porta. Sed porttitor lectus nibh. Pellentesque in ipsum id
-                            orci porta dapibus. Nulla porttitor accumsan tincidunt. Cras ultricies ligula sed magna
-                            dictum porta. Donec sollicitudin molestie malesuada.
-                        </p>
+                        headerComp
                     }
-
                 />
-                <Button buttonText={this.state.subText} buttonHandler={this.onStateSubText.bind(this)}/>
+
+              <Button
+                    buttonHandler={
+                        this.handleClick
+                    }
+                    buttonText={
+                        this.state.isToggleText ? 'Hide Subtitle' : 'Show Subtitle'
+                    }
+                />
+
             </div>
         );
     }
 
 }
+
+
+
 
 render(<App/>, document.getElementById("app"));
--- src/js/components/button.js
+++ src/js/components/button.js
@@ -1,30 +1,12 @@
 import React from 'react';
 
 export default class Button extends React.Component{
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            buttonText: this.props.buttonText
-        };
-    }
-
-
-    onChangeButtonText(){
-
-        if(this.props.buttonText == "Hide the subtitle text"){
-            this.setState({
-                buttonText: "Show subtitle text"
-            });
-        }
-        this.props.buttonHandler(this.state.buttonText);
-    }
-
-
-
     render(){
         return(
-            <button onClick={this.onChangeButtonText.bind(this)}>{this.state.buttonText}</button>
+            <button id="button" onClick={this.props.buttonHandler}>
+                {this.props.buttonText}
+            </button>
         );
     }
 }
+
